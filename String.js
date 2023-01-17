db0e98fdc4005f101dfcccefe9524248561c370f
musharraf ansari <musharrafansari>
2023-01-17 11:59:28 +0530

    done

--- String.js
+++ String.js
@@ -133,3 +133,71 @@ function solve(nums2,nums1){
     return res
   }
   console.log(solve(nums))
+
+
+//          SEARCH IN ROTATAED ARRAY
+
+let nums = [4,5,6,7,0,1,2], target = 0
+function solve(nums,target){
+    let left=0,right=nums.length-1
+
+    while(left<=right){
+        let mid=Math.floor(left+(right-left)/2)
+        if(nums[mid]==target){
+            return mid
+        }
+        if(nums[left]<=nums[mid]){
+            if(nums[left]<=target && target<nums[mid]){
+                right=mid-1
+            }else{
+                left=mid+1
+            }
+        }else{
+            if(nums[mid]<target && target<=nums[right]){
+                left=mid+1
+            }else{
+                right=mid-1
+            }
+        }
+    }
+    return -1
+}
+console.log(solve(nums,target))
+
+
+///                         Find Peak Element
+
+let nums = [1,2,3,1]
+
+function solve(nums){
+    let left=0,right=nums.length-1
+    while(left<=right){
+        let mid=Math.floor(left+(right-left)/2)
+        if(nums[mid]>nums[mid+1]){
+            right=mid-1
+        }else{
+            left=mid+1
+        }
+    }
+    return left
+}
+console.log(solve(nums))
+
+////                square root of number
+
+let x = 8
+function solve(x){
+  let i=0,j=x
+  let ans=0
+  while(i<=j){
+    let mid=Math.floor(i+(j-i)/2)
+    if(mid*mid<=x){
+      ans=mid
+      i=mid+1
+    }else{
+      j=mid-1
+    }
+  }
+  return ans
+}
+console.log(solve(x))
